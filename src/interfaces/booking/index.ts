be31b0fe0574442696e7a80f118bcc981104b576
roq-ai <>
2023-06-26 16:16:35 +0000

    Initial commit

---
+++ src/interfaces/booking/index.ts
@@ -0,0 +1,23 @@
+import { UserInterface } from 'interfaces/user';
+import { CarInterface } from 'interfaces/car';
+import { GetQueryInterface } from 'interfaces';
+
+export interface BookingInterface {
+  id?: string;
+  start_date: any;
+  end_date: any;
+  customer_id?: string;
+  car_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  user?: UserInterface;
+  car?: CarInterface;
+  _count?: {};
+}
+
+export interface BookingGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  customer_id?: string;
+  car_id?: string;
+}
